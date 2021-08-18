2b34d6f1032ba09fa8463d25df311db8a2093ab4
Lucas Passos <lucas.depassos@yahoo.com.br>
2021-08-18 15:34:22 -0300

    customer add

--- src/controllers/TDTController.js
+++ src/controllers/TDTController.js
@@ -38,15 +38,15 @@ module.exports = {
             
         
     },
-    newClient: async (req, res) => {
+    newCustomer: async (req, res) => {
 
             process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;       
-            const product = req.body            
-            const queryData = Object.values(product)
+            const customer = req.body            
+            const queryData = Object.values(customer)
             console.log(queryData)
-            const newProduct = await pool.query("INSERT INTO products(product_id, product_nome, product_desc, product_marca, product_forn, product_class, product_custo, product_venda, product_estoque, product_loja) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", queryData)
+            const newCustomer = await pool.query("INSERT INTO customers(id, customer_nome, customer_end, customer_buys, customer_tel) VALUES($1, $2, $3, $4, $5)", queryData)
 
-            res.json(newProduct)
+            res.json(newCustomer)
       
             
         
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ router.get('/ping', TDTController.ping)
 router.get('/all', TDTController.all)
 router.get('/users', TDTController.one)
 router.post('/productadd', TDTController.newProduct)
-router.post('/clientadd', TDTController.newClient)
+router.post('/clienteadd', TDTController.newCustomer)
 router.put('/ticket/:id', TDTController.edit)
 router.delete('/ticket/:id', TDTController.delete)
 
